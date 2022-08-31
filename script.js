9b866e2ff160ab3ad6a8353c6f35f8e1d79f7700
Hamdan Salih <hamdansalihpc@gmail.com>
2022-08-31 23:46:37 +0530

    initial commit with html css and js files

---
+++ script.js
@@ -0,0 +1,27 @@
+let num1 = 10;
+let num2 = 25;
+function addition(){
+    let sum =  num1 + num2;
+    console.log(sum);
+}
+
+function subtraction(){
+    let difference = num1 - num2;
+    console.log(difference);
+}
+
+function multiplication(){
+    let product = num1 * num2
+    console.log(product);
+}
+
+function division() {
+    let divide = num1 / num2;
+    console.log(divide);
+}
+
+addition();
+subtraction();
+multiplication();
+division();
+
